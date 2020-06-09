66393ea210795f8790ac08a65593833abf043a9f
lawyerphx <panhexiang@pingcap.com>
2020-06-09 04:40:05 -0700

    add nth_plan

--- misc.go
+++ misc.go
@@ -865,6 +865,7 @@ var hintTokenMap = map[string]int{
 	"USE_TOJA":                hintUseToja,
 	"TIME_RANGE":              hintTimeRange,
 	"USE_CASCADES":            hintUseCascades,
+	"NTH_PLAN":                hintNthPlan,
 
 	// TiDB hint aliases
 	"TIDB_HJ":   hintHashJoin,
